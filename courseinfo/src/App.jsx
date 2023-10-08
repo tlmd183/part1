4e62623ba04a147805a7ef3dae24e66ad9d35dd0
pyon <tlmd183@gmail.com>
2023-10-08 22:27:08 +0200

    Change the course into a single JavaScript object

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,33 +1,3 @@
-const Header = (props) => {
-
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
-
-const Content = (props) => {
- 
-  return (
-    <div>
-      <p>{props.parts[0].name} {props.parts[0].exercises}</p>
-      <p>{props.parts[1].name} {props.parts[1].exercises}</p>
-      <p>{props.parts[2].name} {props.parts[2].exercises}</p>
-    </div>
-  )
-  
-}
-
-const Total = (props) => {
-  
-  return (
-    <div>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    </div>
-  )
-}
-
 const App = () => {
   const course = 'Half Stack application development'
   const parts = [ 
@@ -47,9 +17,17 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course}/>  
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <div>
+        <h1>{course}</h1>
+      </div>
+      <div>
+        <p>{parts[0].name} {parts[0].exercises}</p>
+        <p>{parts[1].name} {parts[1].exercises}</p>
+        <p>{parts[2].name} {parts[2].exercises}</p>
+      </div>    
+      <div>
+        <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+      </div>
     </div>
   )
 }
