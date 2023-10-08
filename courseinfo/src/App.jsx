745d60fd7091290fe52be4d367ba6ab25223da16
pyon <tlmd183@gmail.com>
2023-10-08 22:14:53 +0200

    Place objects into an array and modify application accordingly for exercise 1.4

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -11,9 +11,9 @@ const Content = (props) => {
  
   return (
     <div>
-      <p>{props.part1.name} {props.part1.exercises}</p>
-      <p>{props.part2.name} {props.part2.exercises}</p>
-      <p>{props.part3.name} {props.part3.exercises}</p>
+      <p>{props.parts[0].name} {props.parts[0].exercises}</p>
+      <p>{props.parts[1].name} {props.parts[1].exercises}</p>
+      <p>{props.parts[2].name} {props.parts[2].exercises}</p>
     </div>
   )
   
@@ -23,31 +23,33 @@ const Total = (props) => {
   
   return (
     <div>
-      <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     </div>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
+  const parts = [ 
+    {
     name: 'Fundamentals of React', 
     exercises: 10
-  }
-  const part2 = {
+    },
+    {
     name: 'Using props to pass data', 
     exercises: 7
-  }
-  const part3 = {
+    },
+    {
     name: 'State of a component',
     exercises: 14
-  }
+    }
+  ]
 
   return (
     <div>
       <Header course={course}/>  
-      <Content part1={part1} part2={part2} part3={part3}/>
-      <Total part1={part1} part2={part2} part3={part3}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
